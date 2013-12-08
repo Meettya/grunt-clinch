d452e0dadbf49bd8389e3605c4499a9321ae3437
Karpich Dmitry <meettya@gmail.com>
2013-12-08 11:56:47 +0300

    Отписался по замечаниям

--- tasks/clinch.js
+++ tasks/clinch.js
@@ -23,15 +23,15 @@ var path           = require('path');
  * Clinch compiler
  */
 var Compilation = function (package_config, done) {
-  this.clinch = new Clinch();
+  this.clinch = new Clinch(); // вот это стоит вынести наверх - иначе замечательный кеш, который я полировал почти неделю - не используется
   this.compile(package_config, done);
 };
 
 
 Compilation.prototype.compile = function (package_config, done) {
   this.clinch.buldPackage(package_config, function (err, data) {
-    if (err)  console.log(err);
-    fs.writeFileSync(package_config.out, data);
+    if (err)  console.log(err); // не уверен что в случае ошиби достаточно просто отписаться в лог - может done умеет понимать ошибку?
+    fs.writeFileSync(package_config.out, data); // а почему Sync?
     done && done();
   });
 };
